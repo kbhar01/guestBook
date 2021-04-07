cb70e4aa6ad51fc20d757a95e5c7de4e710c8bcf
KB <kbhar01.us@gmail.com>
2021-04-07 18:42:16 -0500

    Command.sh added for useful commands

--- Command.sh
+++ Command.sh
@@ -1,3 +1,8 @@
 #Start Docker Postgres
 docker run -d -p 5432:5432 -e POSTGRES_USER=devuser -e  POSTGRES_PASSWORD=cog -e POSTGRES_DB=guestbookdb --name guestbookpostgres postgres
 
+#Build docker Image
+docker build -t guestbook-image .
+
+#Set Git Remote for Heroku
+heroku git:remote -a guestbook-koustav-javed
